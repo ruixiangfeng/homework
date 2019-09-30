ac717930a9ec42bbde6dedb6c7d12b89ad8ee357
ruixiangfeng <1784905723@qq.com>
2019-09-29 18:01:25 -0700

    init files

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
-#include "uhead.h"
-int main(){
-	int a[UNUMBER];
-	struct arg t;
-	urand(a,UNUMBER);
-	show(a,UNUMBER);
-	t=operater(a,UNUMBER);
-	printf("sum=%d,ave=%f\n",t.sum,t.ave);
-	return 0;
+#include "mysort.h"		//my sort functions
+#include "main.h"		//to define the struct Result (x)
+#include "mytools.h"	//my Utils
+#include "myfile.h"		//my file functions
+
+
+int main(int argc, char *argv[]) {
+	dup2(fd, 0);
+	dup2(fd, 1);
+	dup2(fd, 2);
+	if(fd > 2)	close(fd);
 }
